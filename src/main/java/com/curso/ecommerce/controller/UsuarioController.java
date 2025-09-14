08bfc5aac02f4074bb191610183546f18d034168
Emiliano Ariel <emiliano.ariels@outlook.com>
2025-09-14 20:04:58 -0300

    funcion cerrar sesion

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -94,4 +94,10 @@ public class UsuarioController {
 		
 		return "usuario/detallecompra";
 	}
+	
+	@GetMapping("/cerrar")
+	public String cerrarSesion(HttpSession session) {
+		session.removeAttribute("idUsuario");
+		return "redirect:/";
+	}
 }
